06d47185f22671017857d5f021e10c5bc2d180d2
sanzanatahsin <94426834+sanzanatahsin@users.noreply.github.com>
2023-02-09 23:54:36 +0700

    Add files via upload

---
+++ 0c428ae2-813c05d411faa0082614.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[13],{3750:function(t,a,r){r.d(a,{Ovj:function(){return u}});var e=r(4405);function u(t){return(0,e.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"}}]})(t)}}}]);
+//# sourceMappingURL=0c428ae2-813c05d411faa0082614.js.map
